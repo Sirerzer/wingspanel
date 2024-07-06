11fbf44b54c4403ad6999c3e10b8caed0dbec406
Sirerzer <137716831+Sirerzer@users.noreply.github.com>
2024-07-06 14:43:48 +0200

    Add files via upload

--- yml.py
+++ yml.py
@@ -3,19 +3,28 @@ from datetime import *
 current_time = datetime.now().strftime('%H:%M:%S')
 
 print(f"[{current_time}] load the config")
-with open("config.yml", 'r') as stream:
-    try:
-        # Chargement du fichiers
-        config = yaml.load(stream,Loader=yaml.FullLoader)
+try:
+    with open("config.yml", 'r') as stream:
+        try:
+            # Chargement du fichiers
+            config = yaml.load(stream,Loader=yaml.FullLoader)
 
-        # Récupération des personnes
-        port = config["port"]
-        debug = config["debug"]
-        ACCESS_TOKEN = config["token"]
-        # Lecture des personnes une à une
-        current_time = datetime.now().strftime('%H:%M:%S')
+            # Récupération des personnes
+            port = config["port"]
+            debug = config["debug"]
+            ACCESS_TOKEN = config["token"]
+            remote = config["remote"]
+            # Lecture des personnes une à une
+            current_time = datetime.now().strftime('%H:%M:%S')
 
-        print(f"[{current_time}] finish to loading the config")
-    except yaml.YAMLError as ex:
-        print("YAML FILE HAS SYNTAX ERROR :")
-        print(ex)
+            print(f"[{current_time}] finish to loading the config")
+        except yaml.YAMLError as ex:
+            print("YAML FILE HAS SYNTAX ERROR :")
+            print(ex)
+except FileNotFoundError:
+    print('''
+              -------------------------------------------------------------------------------
+              Wings est a configuré via le panel allez dans nodes puis config pui auto deploy
+              ou copier coller le contenue
+              -------------------------------------------------------------------------------
+    ''')
