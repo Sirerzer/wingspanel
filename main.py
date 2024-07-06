05cc9f81f6433acb5f5b5e490b37d7652b68486c
Sirerzer <137716831+Sirerzer@users.noreply.github.com>
2024-07-06 15:19:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 print('''
                      ____
 __  WebPanel   _____/___/_______ _______ ______
-\_____\    \/\/    /   /       /  __   /   ___/
-   \___\          /   /   /   /  /_/  /___   /
-        \___/\___/___/___/___/___    /______/
-                            /_______/ 0.0.3
+\\_____\\    \\/\\/    /   /       /  __   /   ___/
+   \\___\\          /   /   /   /  /_/  /___   /
+        \\___/\\___/___/___/___/___    /______/
+                            /_______/ 0.0.6
 ''')
+
 import db
 import yml
 import listen
 
 
 
-
-
-
-listen.run(debug=yml.debug,port=yml.port)
+if(yml.port == ''):
+   print("Programe finish")
+else:
+   listen.run(debug=yml.debug,port=yml.port)
