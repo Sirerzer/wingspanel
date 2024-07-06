aac2772ea3f1d510281b18f8579c17aed99018c6
Sirerzer <137716831+Sirerzer@users.noreply.github.com>
2024-07-06 15:37:29 +0200

    Add files via upload

--- tests/test_yml.py
+++ tests/test_yml.py
@@ -5,5 +5,5 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import yml  # Now it should find yml.py in the parent directory
 
 def test_yml():
-    var = yml.test_yml()
-    print(vars(var))
+    yml.test_yml()
+    
